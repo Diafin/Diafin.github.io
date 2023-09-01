edec5a7d9b15fa7d1cee5acb15039c1ffa387898
dvereschagin <dvereschagin@lachestry.com>
2023-09-01 16:21:22 +0300

    Made templates for page
    [#2137]

---
+++ src/js/init-page.js
@@ -0,0 +1,21 @@
+if (typeof youplay !== 'undefined') {
+    youplay.init
+    (
+        {
+        // enable parallax
+            parallax:         true,
+
+            // set small navbar on load
+            navbarSmall:      false,
+
+            // enable fade effect between pages
+            fadeBetweenPages: true,
+
+            // twitter and instagram php paths
+            php: {
+                twitter: './php/twitter/tweet.php',
+                instagram: './php/instagram/instagram.php'
+            }
+        }
+    );
+}
